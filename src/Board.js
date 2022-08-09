9536d91b11de94dcdafa9b906185242e421bc783
Akpjunior94 <akpjunio4real@gmail.com>
2022-08-09 11:03:08 +0100

    ✨ Moving shipping request status

--- src/Board.js
+++ src/Board.js
@@ -1,59 +1,79 @@
-import React from 'react';
-import Dragula from 'dragula';
-import 'dragula/dist/dragula.css';
-import Swimlane from './Swimlane';
-import './Board.css';
-
+import React from "react";
+// import dragula from "dragula";
+import "dragula/dist/dragula.css";
+import Swimlane from "./Swimlane";
+import "./Board.css";
+import Dragula from "dragula";
+import { swimlanesData } from "./swimlanes.data";
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
-    const clients = this.getClients();
+    let clients = this.getClients();
     this.state = {
       clients: {
-        backlog: clients.filter(client => !client.status || client.status === 'backlog'),
-        inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
-        complete: clients.filter(client => client.status && client.status === 'complete'),
-      }
-    }
+        backlog: clients.filter(
+          (client) => !client.status || client.status === "backlog"
+        ),
+        inProgress: clients.filter(
+          (client) => client.status && client.status === "in-progress"
+        ),
+        complete: clients.filter(
+          (client) => client.status && client.status === "complete"
+        ),
+      },
+      data: clients,
+    };
     this.swimlanes = {
       backlog: React.createRef(),
       inProgress: React.createRef(),
       complete: React.createRef(),
-    }
+    };
   }
   getClients() {
-    return [
-      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
-      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'complete'],
-      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'in-progress'],
-      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'in-progress'],
-      ['6','Boehm and Sons','Automated Systematic Paradigm', 'backlog'],
-      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy', 'backlog'],
-      ['8','Schumm-Labadie','Operative Heuristic Challenge', 'backlog'],
-      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-      ['10','Romaguera Inc','Managed Foreground Toolset', 'backlog'],
-      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'complete'],
-      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability', 'backlog'],
-      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'complete'],
-      ['14','Borer LLC','Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'in-progress'],
-      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'in-progress'],
-      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'complete'],
-      ['18','Bins, Toy and Klocko','Integrated Assymetric Software', 'backlog'],
-      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline', 'backlog'],
-      ['20','Murphy, Lang and Ferry','Organized Explicit Access', 'backlog'],
-    ].map(companyDetails => ({
+    return swimlanesData.map((companyDetails) => ({
       id: companyDetails[0],
       name: companyDetails[1],
       description: companyDetails[2],
       status: companyDetails[3],
     }));
   }
+  componentDidMount() {
+    const drake = Dragula([
+      this.swimlanes.complete.current,
+      this.swimlanes.backlog.current,
+      this.swimlanes.inProgress.current,
+    ]);
+    // listening for the drop event and using the following data to change status of card
+    drake.on("drop", (el, target, source, sibling) => {
+      const elId = el.dataset.id;
+      const siblingStatus = sibling && sibling.dataset.status;
+      let swimlanesHeader = target.parentNode.childNodes[0].innerText;
+
+      // find the element id and changing its status to that of the target
+      let droppedData = this.state.data.find(
+        (client) => client.id === elId.toString()
+      );
+      if (sibling === null) {
+        if (swimlanesHeader === "In Progress") {
+          swimlanesHeader = swimlanesHeader.replace(
+            swimlanesHeader,
+            "in-progress"
+          );
+          droppedData.status = swimlanesHeader;
+        }
+        droppedData.status = swimlanesHeader.toLowerCase();
+      } else {
+        droppedData.status = siblingStatus;
+      }
+
+      this.setState({
+        data: [...this.state.data, droppedData],
+      });
+    });
+  }
+
   renderSwimlane(name, clients, ref) {
-    return (
-      <Swimlane name={name} clients={clients} dragulaRef={ref}/>
-    );
+    return <Swimlane name={name} clients={clients} dragulaRef={ref} />;
   }
 
   render() {
@@ -62,13 +82,25 @@ export default class Board extends React.Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-4">
-              {this.renderSwimlane('Backlog', this.state.clients.backlog, this.swimlanes.backlog)}
+              {this.renderSwimlane(
+                "Backlog",
+                this.state.clients.backlog,
+                this.swimlanes.backlog
+              )}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('In Progress', this.state.clients.inProgress, this.swimlanes.inProgress)}
+              {this.renderSwimlane(
+                "In Progress",
+                this.state.clients.inProgress,
+                this.swimlanes.inProgress
+              )}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('Complete', this.state.clients.complete, this.swimlanes.complete)}
+              {this.renderSwimlane(
+                "Complete",
+                this.state.clients.complete,
+                this.swimlanes.complete
+              )}
             </div>
           </div>
         </div>
